c54c06df107079c40c33d48dd2c6eea074859687
Kamran <112368797+KamranRaza12@users.noreply.github.com>
2023-04-23 11:52:33 +0530

    new updates

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -5,11 +5,12 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstTest {
 
 
-    @Test
+   /* @Test
     public void OpenBrowser()  {
         WebDriver driver;
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
@@ -21,6 +22,17 @@ public class FirstTest {
         System.out.println("Title of the page is: " + driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
         //kamran
+    }*/
+	
+    @Test
+    public void OpenBrowser()  {
+    	System.setProperty("webdriver.gecko.driver", ".\\Drivers\\geckodriver.exe");
+    	WebDriver driver = new FirefoxDriver();
+    	String url = "https://nxtgenaiacademy.com";
+    	driver.get(url);
+    	driver.manage().window().maximize();
+    	System.out.println("Driver Launched Successfully");
+    	driver.close();
     }
 
 
