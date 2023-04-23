c037577c8e5d6b2f08214383c031267975f63506
Kamran <112368797+KamranRaza12@users.noreply.github.com>
2023-04-23 13:29:30 +0530

    comment added

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -19,6 +19,7 @@ public class FirstTest {
         driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
+        System.out.println("My name is Kamran");
 		driver.close();
     
     }
