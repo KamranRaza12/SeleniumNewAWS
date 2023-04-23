c9154a17d54b46d0e92fb2224bf671cbde3e26ad
Kamran <112368797+KamranRaza12@users.noreply.github.com>
2023-04-23 13:08:51 +0530

    Update FirstTest.java
    new build

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -4,25 +4,11 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class FirstTest {
 
 
-   /* @Test
-    public void OpenBrowser()  {
-        WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        options.addArguments("disable-gpu");
-        driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
-        System.out.println("Title of the page is: " + driver.getTitle());
-        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
-        //kamran
-    }*/
-	
     @Test
     public void OpenBrowser()  {
     	WebDriver driver;
@@ -33,6 +19,7 @@ public class FirstTest {
         driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
+        System.out.println("New updates on headless brouser");
 		driver.close();
     
     }
