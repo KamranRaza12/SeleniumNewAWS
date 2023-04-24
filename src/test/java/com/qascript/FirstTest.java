b03a2374498dd00588196b88ae1f68ea705b4d3d
Kamran <112368797+KamranRaza12@users.noreply.github.com>
2023-04-24 11:37:16 +0530

    24-04/updates

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -20,6 +20,7 @@ public class FirstTest {
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
         System.out.print("New Updates to be printed on console");
+        System.out.println("Build check 24-04-2023");
 		driver.close();
     
     }
