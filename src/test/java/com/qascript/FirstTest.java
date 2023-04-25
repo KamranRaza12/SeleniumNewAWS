a26122dd1da34d3287056ca7eac1dd3e3ddaaae2
Kamran <112368797+KamranRaza12@users.noreply.github.com>
2023-04-25 20:55:24 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -13,12 +13,7 @@ public class FirstTest {
   @Test
     public void OpenBrowser()  {
     	WebDriver driver;
-    	
-    	//String chromeDriverPath = System.getenv("CHROMEDRIVER_PATH");
-    	//System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-    	//driver = new ChromeDriver();
-
-    	
+      	
     	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
     	ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
@@ -35,26 +30,6 @@ public class FirstTest {
     }
   
 
-   /* @Test
-    public void OpenBrowserNew()  {
-    	WebDriver driver;
-    	//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kamran\\Downloads\\chromedriver.exe");
-    	//ChromeOptions options = new ChromeOptions();
-        //options.addArguments("headless");
-        //options.addArguments("disable-gpu");
-        //driver = new ChromeDriver(options);
-    	//System.setProperty("webdriver.gecko.driver", ".\\Drivers\\geckodriver.exe");
-    	//System.setProperty("webdriver.gecko.driver", ".\\Drivers\\geckodriver.exe");
-    	driver = new FirefoxDriver();
-        driver.get("https://www.google.com");
-        System.out.println("Title of the page is: " + driver.getTitle());
-        System.out.println("New Updates to be printed on console");
-
-        System.out.println("TestCase 2");
-        
-		driver.close();
-    
-    }*/
 }
 
 
