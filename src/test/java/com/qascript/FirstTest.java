2748460c75837cfb3d54b70924a34638604bb78c
Kamran <112368797+KamranRaza12@users.noreply.github.com>
2023-04-25 11:08:01 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -9,30 +9,10 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class FirstTest {
 
 
-  @Test
-    public void OpenBrowser()  {
-    	WebDriver driver;
-    	//System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-    	System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
-    	ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        options.addArguments("disable-gpu");
-        driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
-        System.out.println("Title of the page is: " + driver.getTitle());
-        System.out.println("New Updates to be printed on console");
-
-        System.out.println("TestCase 1");
-        
-		driver.close();
-    
-    }
-    
-
-//    @Test
-//    public void OpenBrowserNew()  {
+//  @Test
+//    public void OpenBrowser()  {
 //    	WebDriver driver;
-//    	System.setProperty("webdriver.chrome.driver", ".\\Drivers\\chromedriver.exe");
+//    	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 //    	ChromeOptions options = new ChromeOptions();
 //        options.addArguments("headless");
 //        options.addArguments("disable-gpu");
@@ -41,11 +21,30 @@ public class FirstTest {
 //        System.out.println("Title of the page is: " + driver.getTitle());
 //        System.out.println("New Updates to be printed on console");
 //
-//        System.out.println("TestCase 2");
+//        System.out.println("TestCase 1");
 //        
 //		driver.close();
 //    
 //    }
+//    
+
+    @Test
+    public void OpenBrowserNew()  {
+    	WebDriver driver;
+    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kamran\\OneDrive\\Documents\\GitHub\\SeleniumNewAWS\\Drivers\\ChromeDriver.exe");
+    	ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("disable-gpu");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+        System.out.println("Title of the page is: " + driver.getTitle());
+        System.out.println("New Updates to be printed on console");
+
+        System.out.println("TestCase 2");
+        
+		driver.close();
+    
+    }
 }
 
 
