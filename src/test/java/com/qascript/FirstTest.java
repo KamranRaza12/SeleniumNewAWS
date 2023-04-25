f1773053b7afb07945d0d5b736d9df4089b77696
Kamran <112368797+KamranRaza12@users.noreply.github.com>
2023-04-25 13:16:14 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -13,13 +13,12 @@ public class FirstTest {
   @Test
     public void OpenBrowser()  {
     	WebDriver driver;
-    	//System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-    	//ChromeOptions options = new ChromeOptions();
-        //options.addArguments("headless");
-        //options.addArguments("disable-gpu");
-        //driver = new ChromeDriver(options);
-    	System.setProperty("webdriver.gecko.driver", "/usr/bin/firefox");
-    	driver = new FirefoxDriver();
+    	
+    	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+    	ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("disable-gpu");
+        driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
         System.out.println("New Updates to be printed on console");
